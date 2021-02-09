849b5bf04c5fe267c9244a745ede874e9027e8b1
andresmauricio <andresacelas@outlook.com>
2021-02-08 22:05:35 -0500

    refactor de propiedades y metodos

--- projects/bmi/src/app/components/result/result.component.ts
+++ projects/bmi/src/app/components/result/result.component.ts
@@ -4,33 +4,33 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
-  styleUrls: ['./result.component.scss']
+  styleUrls: ['./result.component.scss'],
 })
 export class ResultComponent implements OnInit {
   bmi: number;
-  resultado: string;
-  interpretacion: string;
+  result: string;
+  description: string;
 
   constructor(private aRoute: ActivatedRoute) {
     this.bmi = +aRoute.snapshot.paramMap.get('value');
-   }
+  }
 
   ngOnInit(): void {
-    this.getResultado();
+    this.getResult();
   }
 
-  getResultado(): void {
-    
+  getResult(): void {
     if (this.bmi >= 25) {
-      this.resultado = 'Exceso de peso';
-      this.interpretacion = 'Tienes un peso corporal superior al normal. Intente hacer más ejercicio.';
+      this.result = 'Exceso de peso';
+      this.description =
+        'Tienes un peso corporal superior al normal. Intente hacer más ejercicio.';
     } else if (this.bmi >= 18.5) {
-      this.resultado = 'Normal';
-      this.interpretacion = 'Tienes un peso corporal normal. ¡Buen trabajo!';
+      this.result = 'Normal';
+      this.description = 'Tienes un peso corporal normal. ¡Buen trabajo!';
     } else {
-      this.resultado = 'Bajo peso';
-      this.interpretacion = 'Tienes un peso corporal más bajo de lo normal. Puedes comer un poco más.';
+      this.result = 'Bajo peso';
+      this.description =
+        'Tienes un peso corporal más bajo de lo normal. Puedes comer un poco más.';
     }
   }
-
 }
