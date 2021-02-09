725df320ef74415d25c7a60c132454b5e7726e2d
andresmauricio <andresacelas@outlook.com>
2021-02-08 21:49:29 -0500

    formulario de indice de masa corporal

--- projects/bmi/src/app/components/home/home.component.ts
+++ projects/bmi/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  edad = 25;
+  peso = 60;
+  altura = 170;
+  sexo = 'Masculino';
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  cambiarAltura(event: any): void{
+    console.log(event);
+    this.altura = event.target.value;
+  }
+
+  masculino(): void {
+    this.sexo = 'Masculino';
+  }
+
+  femenino(): void {
+    this.sexo = 'Femenino';
+  }
+
+  calcular(): void {
+    const BMI = this.peso / Math.pow(this.altura / 100, 2);
+    this.router.navigate(['result', BMI.toFixed(1)]);
   }
 
 }
