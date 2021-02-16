6091879d3824e1b119560ec2239e50cd1193b1ca
andresmauricio <andresacelas@outlook.com>
2021-02-15 21:20:58 -0500

    binding of data

--- projects/bitly/src/app/components/dashboard/dashboard.component.ts
+++ projects/bitly/src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+const constains = {
+  buttonShortcut: 'Shorcut',
+  buttonReset: 'Reset',
+}
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
+  buttonLabel = constains.buttonShortcut;
+  urlOrigin = '';
+  urlResult = '';
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  transform() {
+    console.log('transform');
+    
   }
 
 }
