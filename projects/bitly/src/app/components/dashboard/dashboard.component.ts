f79152178170daf38054b0dbdf648430306d6637
andresmauricio <andresacelas@outlook.com>
2021-02-15 21:24:42 -0500

    initial method

--- projects/bitly/src/app/components/dashboard/dashboard.component.ts
+++ projects/bitly/src/app/components/dashboard/dashboard.component.ts
@@ -3,24 +3,27 @@ import { Component, OnInit } from '@angular/core';
 const constains = {
   buttonShortcut: 'Shorcut',
   buttonReset: 'Reset',
-}
+};
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
   buttonLabel = constains.buttonShortcut;
   urlOrigin = '';
   urlResult = '';
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   transform() {
-    console.log('transform');
-    
+    if (this.buttonLabel === constains.buttonShortcut) {
+      this.buttonLabel = constains.buttonReset;
+    } else {
+      this.buttonLabel = constains.buttonShortcut;
+      this.urlOrigin = '';
+      this.urlResult = '';
+    }
   }
-
 }
