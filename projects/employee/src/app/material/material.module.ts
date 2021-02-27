de1852ca57bc32301c129c80c97eb26903ec441d
andresmauricio <andresacelas@outlook.com>
2021-02-26 22:44:40 -0500

    paginator and sort dataSources

--- projects/employee/src/app/material/material.module.ts
+++ projects/employee/src/app/material/material.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatToolbarModule, MatTableModule],
-  exports: [MatToolbarModule, MatTableModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+  ],
+  exports: [
+    MatToolbarModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+  ],
 })
 export class MaterialModule {}
