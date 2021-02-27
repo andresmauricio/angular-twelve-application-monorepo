2c55370c8875fc8e32bfbb1884968a232934c3d9
andresmauricio <andresacelas@outlook.com>
2021-02-26 21:55:12 -0500

    component list of employees

--- projects/employee/src/app/app.module.ts
+++ projects/employee/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListEmployeesComponent } from './components/list-employees/list-employees.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ListEmployeesComponent
   ],
   imports: [
     BrowserModule,
