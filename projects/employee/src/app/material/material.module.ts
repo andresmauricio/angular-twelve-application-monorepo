8649ccc8f4fecacaeafb6130085c0dd1e071fd82
andresmauricio <andresacelas@outlook.com>
2021-02-26 22:04:34 -0500

    component table

--- projects/employee/src/app/material/material.module.ts
+++ projects/employee/src/app/material/material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatToolbarModule],
-  exports: [MatToolbarModule]
+  imports: [CommonModule, MatToolbarModule, MatTableModule],
+  exports: [MatToolbarModule, MatTableModule],
 })
 export class MaterialModule {}
