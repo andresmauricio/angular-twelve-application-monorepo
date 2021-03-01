e6d8b17e109ab577dc77c403ed5fc0e0893f8e31
andresmauricio <andresacelas@outlook.com>
2021-02-28 20:16:00 -0500

    router create and edit employee

--- projects/employee/src/app/app-routing.module.ts
+++ projects/employee/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from 'projects/bitly/src/app/app.component';
+import { CreateEditEmployeeComponent } from './components/create-edit-employee/create-edit-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'create', component: CreateEditEmployeeComponent },
+  { path: 'edit:/id', component: CreateEditEmployeeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
