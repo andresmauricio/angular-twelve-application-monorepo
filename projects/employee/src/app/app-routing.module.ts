0290b73115f4868207a27a229d3c1226dbf21944
andresmauricio <andresacelas@outlook.com>
2021-02-28 20:29:41 -0500

    make ui form whit-out grid

--- projects/employee/src/app/app-routing.module.ts
+++ projects/employee/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from 'projects/bitly/src/app/app.component';
 import { CreateEditEmployeeComponent } from './components/create-edit-employee/create-edit-employee.component';
 
 const routes: Routes = [
